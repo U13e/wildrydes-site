3d9807b70fcabeb66e01bbb980ce186adfe7b2d9
U13e <49995912+U13e@users.noreply.github.com>
2025-03-25 21:02:34 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_AqBAvRNbb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'vjt04fdo2d7imc63jaa4pc2ab', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
